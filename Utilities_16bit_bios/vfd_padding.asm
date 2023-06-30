776919867d206788efb8c74e0edfef100ef576fd
Alexander Hunt <alexander.l.hunt951@gmail.com>
2023-06-30 14:42:04 -0400

    Improved VFD Padding

--- Utilities_16bit_bios/vfd_padding.asm
+++ Utilities_16bit_bios/vfd_padding.asm
@@ -1,6 +1,9 @@
-; zero padding and magic bios boot number
+; Zero padding and magic bios boot number
 times 510-($-$$) db 0
 dw 0xaa55
 
-;  makes a 1.44MB VFD
-times 1440*1024/16 - 512/16 dw 0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
+; Align to Memory
+times 16 - ($-$$)% 16 db 0
+
+; Makes a 1.44MB VFD
+times 1440*1024/16 - ($-$$)16 dw 0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
